bc5af0563f19f475e556a81bd2891d397291cf39
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-03-24 01:08:58 -0300

    aula 20- react hooks 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+
 
 function App() {
+
+  const[tarefas, setTarefas] = useState([
+    'Acordar cedo',
+    'Estudar JSES6+',
+    'Lavar o carro',
+    'Estudar Hooks'
+  ]);
+
+  const [campo, setCampo] = useState('');
+
+  const [placeholder, setPlaceHolder] = useState("Digite sua tarefa");
+
+  const adicionarItem = () =>{
+    setTarefas([...tarefas, campo]);	
+    setCampo("");
+
+  }
+
+  const handleCampo = (e) =>{
+    setCampo(e.target.value);
+  }
+
+  const limparPlaceHolder = () =>{
+    setPlaceHolder(" ");
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <h1>Pedro Ferreira</h1>
+
+    <ul>
+      {
+        tarefas.map((item, index) =>{
+          return(
+            <li key={index}>{item}</li>
+          )
+        })
+      }
+    </ul>
+
+    <input type="text" value={campo} name="nome" placeholder={placeholder} onChange={handleCampo} />
+    <button type='button' id='add-btn' disabled={campo.length < 3 } onClick={adicionarItem}>Adicionar</button>
     </div>
   );
 }
